2f8b5281840e3e3cc8db4a750f5320ca6fb9a09e
Anuradha <omananu4@gmail.com>
2022-12-06 17:42:08 +0530

    Added comments in app.js file

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const port = 6000
 const app = express()
 app.use(express.json())
 
+//get the user data from json file
+app.get('/list', (req, res) => {
+    const users = getUserData()
+    res.send(users)
+})
+
 app.post('/add', (req, res) => {
     const existUsers = getUserData()
     const userData = req.body
@@ -15,18 +21,17 @@ app.post('/add', (req, res) => {
     if (findExist) {
         return res.status(409).send({error: true, msg: 'id  already exist'})
     }
+
+     //append the user data
     existUsers.push(userData)
     saveUserData(existUsers);
     res.send({success: true, msg: 'User data added successfully'})
 })
 
-app.get('/list', (req, res) => {
-    const users = getUserData()
-    res.send(users)
-})
 
 app.patch('/update/:id', (req, res) => {
-    //get the username from url
+   
+    //get the id from url
     const id = req.params.id
 
     //get the update data
@@ -36,16 +41,21 @@ app.patch('/update/:id', (req, res) => {
     if (!findExist) {
         return res.status(409).send({error: true, msg: 'id not exist'})
     }
-   const updateUser = existUsers.filter( user => user.id !== id )
-   updateUser.push(userData)
-   saveUserData(updateUser)
-   res.send({success: true, msg: 'User data updated successfully'})
+    const updateUser = existUsers.filter( user => user.id !== id )
+
+   //push the updated data
+    updateUser.push(userData)
+    
+    //finally save it
+    saveUserData(updateUser)
+    res.send({success: true, msg: 'User data updated successfully'})
 })
 
 app.delete('/delete/:id', (req, res) => {
     const id = req.params.id
     const existUsers = getUserData()
     const filterUser = existUsers.filter( user => user.id !== id )
+
     if ( existUsers.length === filterUser.length ) {
         return res.status(409).send({error: true, msg: 'id does not exist'})
     }
