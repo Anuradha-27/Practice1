2de5cf088a66608f4e01cf505a6d10e57f7f98a8
Anuradha <omananu4@gmail.com>
2022-12-08 12:47:21 +0530

    Added extra function in app.js file

--- app.js
+++ app.js
@@ -3,20 +3,38 @@ const fs = require('fs')
 const port = 6000
 const app = express()
 app.use(express.json())
-
+const { v4: uuidv4 } = require('uuid');
 //get the user data from json file
-app.get('/list', (req, res) => {
+
+app.get('/users', (req, res) => {
     const users = getUserData()
     res.send(users)
 })
 
-app.post('/add', (req, res) => {
+app.get('/users/:id', (req, res) => {
+    
+    const id = req.params.id
     const existUsers = getUserData()
-    const userData = req.body
-
-    if (userData.id == null ||userData.email == null || userData.age == null || userData.username == null || userData.password == null) {
-        return res.status(401).send({error: true, msg: 'User data missing'})
+    const findExist = existUsers.find( user => user.id === id)
+    if (!findExist) {
+        return res.status(409).send({error: true, msg: 'id not exist'})
     }
+    else {
+        res.send(findExist)
+    }
+})
+app.post('/users', (req, res) => {
+    const existUsers = getUserData()
+    const id = uuidv4()
+   //const userData = req.body
+    const userData= {
+         ...req.body,
+        "id":uuidv4()
+    }
+    console.log(userData)
+   if (userData.email == null || userData.age == null || userData.username == null || userData.password == null||userData.id == null) {
+        return res.status(401).send({error: true, msg: 'User data missing'})
+    } 
     const findExist = existUsers.find( user => user.id === userData.id )
     if (findExist) {
         return res.status(409).send({error: true, msg: 'id  already exist'})
@@ -25,11 +43,11 @@ app.post('/add', (req, res) => {
      //append the user data
     existUsers.push(userData)
     saveUserData(existUsers);
-    res.send({success: true, msg: 'User data added successfully'})
+    res.send({success: true, msg: 'User data added successfully',id})
 })
 
 
-app.patch('/update/:id', (req, res) => {
+app.patch('/users/:id', (req, res) => {
    
     //get the id from url
     const id = req.params.id
@@ -37,11 +55,11 @@ app.patch('/update/:id', (req, res) => {
     //get the update data
     const userData = req.body
     const existUsers = getUserData()
-    const findExist = existUsers.find( user => user.id === id )
+    const findExist = existUsers.find( user => user.id === id)
     if (!findExist) {
         return res.status(409).send({error: true, msg: 'id not exist'})
     }
-    const updateUser = existUsers.filter( user => user.id !== id )
+    const updateUser = existUsers.filter( user => user.id !== id)
 
    //push the updated data
     updateUser.push(userData)
@@ -51,7 +69,7 @@ app.patch('/update/:id', (req, res) => {
     res.send({success: true, msg: 'User data updated successfully'})
 })
 
-app.delete('/delete/:id', (req, res) => {
+app.delete('/users/:id', (req, res) => {
     const id = req.params.id
     const existUsers = getUserData()
     const filterUser = existUsers.filter( user => user.id !== id )
